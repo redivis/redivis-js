61e9bb1072abf35da5f71ecd195f0ae6c9fc2b5a
Ian Mathews <ian@redivis.com>
2021-10-04 13:33:33 -0700

    update test

--- test/index.js
+++ test/index.js
@@ -2,8 +2,9 @@ import * as redivis from '../index.js';
 
 // TODO: build out test suite
 async function runTests() {
-	// const queryRows = await redivis.query('SELECT 1+1').listRows();
-	// console.log(queryRows);
+	// await redivis.authorize({ apiToken: process.env.REDIVIS_API_TOKEN });
+	const queryRows = await redivis.query('SELECT 1+1').listRows();
+	console.log(queryRows);
 	//
 	// const table = await redivis.organization('demo').dataset('Novel Corona Virus 2019 Dataset').table('Covid 19 data');
 	//
